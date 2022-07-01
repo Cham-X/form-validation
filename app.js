58f3973407ee1ef9b80da1db16ae8897e3493143
Shamsudeen <zakariyyahshamsudeen@gmail.com>
2022-07-01 22:59:40 +0100

    bug

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 class User{
   constructor( firstname, lastname,email,password,confirmPassword) {
-    this.firstname = firstname,
-    this.lastname = lastname,
-    this.email = email,
-    this.password = password,
-    this.confirmPassword = confirmPassword;
+    this.firstname = firstname.value,
+    this.lastname = lastname.value,
+    this.email = email.value,
+    this.password = password.value,
+    this.confirmPassword = confirmPassword.value;
   }
 }
 
- const firstname = document.querySelector('#firstname'), //.value.trim(),
+ const  firstname = document.querySelector('#firstname'), //.value.trim(),
         lastname = document.querySelector('#lastname'), //.value.trim(),
         email = document.querySelector('#email'),//.value.trim(),
         password = document.querySelector('#password'),//.value.trim(),
@@ -18,92 +18,106 @@ document.getElementById('reg-form').addEventListener('submit', (e) => {
 
    const user = new User( firstname, lastname, email, password, confirmPassword);
   
-   validateFirstname();
-   validateLastname();
-   validateEmail();
-   validatePassword();
-   validateConfirmPassword();
+   var name = document.getElementById('firstname');
+   var surname = document.getElementById('lastname');
+   var re = /^[a-zA-Z]{3,12}$/;
+  
+  if (name.value === '') {
+    setError('All fields must not empty', firstname)
+  } else if(!re.test(name.value)){
+    setError('firstname must contain between 3 t0 12 character', firstname)
+  } else {
+    setSuccess(firstname)
+  }
 
-   e.preventDefault();
-});
-
-document.getElementById('firstname').addEventListener('blur',validateFirstname);
-document.getElementById('lastname').addEventListener('blur',validateLastname);
-document.getElementById('email').addEventListener('blur',validateEmail);
-document.getElementById('password').addEventListener('blur',validatePassword);
-document.getElementById('confirm-password').addEventListener('blur',validateConfirmPassword);
-
-function validateFirstname(){
-  if (firstname.value === ''){
-    setError('All fields must not be empty', firstname)
- }else if ( firstname.value.length < 3 || firstname.length > 12){
-   setError('Firstname must not be less than 3 and greater than 12 characters', firstname )
- }else{
-   setSuccess(firstname)
- }
-}
-function validateLastname(){
   if (lastname.value === ''){
     setError('All fields must not empty', lastname)
-  }else if (lastname.value.length < 3 || lastname.value.length > 12){
-    setError('Lastname must not be less than 3 and greater than 12 characters', lastname )
+  } else if (!re.test(surname.value)){
+    setError('Lastname must contain between 3 to 12 characters', lastname )
   }else{
     setSuccess(lastname)
   }
 
-}
-function validateEmail(){
   if(email.value === ''){
-    setError('All fields must not be empty',email)
+    setError('All fields must not be empty', email)
   }else{
     setSuccess(email)
   }
-}
-function validatePassword(){
+
   if ( password.value === ''){
-    setError('All field must be filled', password)
+    setError('All fields must not be empty', password)
   }else if ( password.value.length < 6){
      setError('Password must not be less than 6 character', password)
   } else {
     setSuccess(password)
   }
-}
-function validateConfirmPassword(){
+
   if (confirmPassword.value === ''){
-    setError('All field must be filled', confirmPassword)
+    setError('All fields must not be empty', confirmPassword)
   }else if (confirmPassword.value !== password.value){
-    setError('Enter password Again', confirmPassword)
+    setError('Confirm password Again', confirmPassword)
   }else{
     setSuccess(confirmPassword)
   }
-}
+
+  setTimeout( setError , 200);
+  
+//  console.log( setError.errorDisplay)
+   e.preventDefault();
+});
+
+// document.getElementById('firstname').addEventListener('blur',validateFirstname);
+// document.getElementById('lastname').addEventListener('blur',validateLastname);
+// document.getElementById('email').addEventListener('blur',validateEmail);
+// document.getElementById('password').addEventListener('blur',validatePassword);
+// document.getElementById('confirm-password').addEventListener('blur',validateConfirmPassword);
+
+// function validateFirstname(){
+  
+// }
+// function validateLastname(){
+ 
+// }
+// function validateEmail(){
+
+// }
+// function validatePassword(){
+
+// }
+// function validateConfirmPassword(){
+  
+// }
 
 const setError = ( message , element ) => {
 
-  const wrapperBorder = element.parentElement,
-        errorDisplay = element.parentElement.nextElementSibling;
+  const wrapperBorder = element.parentElement;
+  const errorDisplay = element.parentElement.nextElementSibling;
 
   errorDisplay.innerText = message;
   wrapperBorder.style.borderColor = 'red';
 
-  setTimeout(function(){errorDisplay.remove(); wrapperBorder.style.borderColor = '#ffff'; 
-}, 4000);
-
+  // setTimeout(function(){errorDisplay.remove();
+  // wrapperBorder.style.borderColor = '#ffff'; }, 4000);
+  
 }
+
 
 const setSuccess = (element) => {
   const wrapperBorder = element.parentElement;
+  const errorDisplay = element.parentElement.nextElementSibling;
 
   wrapperBorder.style.border = '1.3px solid #ffffff';
+  errorDisplay.style.innerText = '';
 
-  alert('Registration successful');
-  clearFields()
+  clearFields();
+  // alert('Registration successful');
+ 
 }
 
 const clearFields = () => {
-  firstname.value = '';
-  lastname.value = '';
-  email.value = '';
-  password.value = '';
+  firstname = '';
+  lastname = '';
+  email = '';
+  password = '';
   confirmPassword = '';
 }
